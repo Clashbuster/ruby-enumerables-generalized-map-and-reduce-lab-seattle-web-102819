71eb083cdf50016e51f1425f67b556459917386a
Taylor Coon <taylor.allan.coon@gmail.com>
2019-10-24 21:45:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,14 +21,10 @@ def reduce(array, sv = nil)
   if sv
     num1 = sv
     i = 0
-  else
-    num1 = array[0]
-    i = 1
   end
 while i < array.length
   sum = yield(num1, array[i])
-i += 1
+ i += 1
 end
 sum
 end
-
